4cee96e00d8da5ef7e03dc87fd38431fa9d4680a
Julio Mariscal <devkoller.jm@gmail.com>
2025-05-26 10:22:02 -0600

    feature: add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [
+		{
+			name: "sigma-server",
+			script: "dist/server.js",
+			ignore_watch: ["node_modules", "public/imgs"],
+			watch: false,
+			max_restarts: 3, // o incluso 0 para testear
+			wait_ready: true,
+			listen_timeout: 8000,
+			max_memory_restart: "1G",
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+}
